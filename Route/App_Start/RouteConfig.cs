b9ee2d458c256cf3e1ee0953efc8ed2b773679d4
xiaoyayun <xiaomufeng789@163.com>
2015-04-21 23:39:24 +0800

    WebProject
    Add Project

---
+++ Route/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Route
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+            null,
+            "{category}", // Matches /Football or /AnythingWithNoSlash
+            new { controller = "Home", action = "Index", page = 1 }
+        );
+
+
+            routes.MapRoute("MyRoute", "{controller}/{action}");
+
+            routes.MapRoute("MyRoute1", "{controller}/{action}", new { controller = "Home", action = "Index" });
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Route/Controllers/BaseControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Route.Controllers
+{
+    public class BaseControl:Controller
+    {
+        protected override void ExecuteCore()
+        {
+            base.ExecuteCore();
+        }
+        protected override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+        }
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+
+            if (filterContext.RouteData.Values.Count>2&& filterContext.RouteData.Values["category"].ToString() == "xiaoyayun")
+            {
+               // RedirectToAction("xiaoyayun", "home");
+                Redirect("/home/xiaoyayun");
+            }
+          //  base.OnActionExecuted(filterContext);
+        }
+    }
+}
